0da66a121d2d9f3a3cf8b1bb972c875e671fdc18
Ca5 <ca54makske@yahoo.co.jp>
2015-07-11 18:25:58 +0900

    facebook追加

--- plugins/sns/sns.py
+++ plugins/sns/sns.py
@@ -1,10 +1,24 @@
-from pelican import signals, contents
-from datetime import datetime
-from pytz import timezone
-import twitter
 import sys
+from HTMLParser import HTMLParser
+import twitter
+import facebook
+from pelican import signals, contents
+from pelican.utils import truncate_html_words
 
+# html tag stripper
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
 
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 class SnsGenerator(object):
 
@@ -12,32 +26,16 @@ class SnsGenerator(object):
 
         self.output_path = output_path
         self.context = context
-        self.now = datetime.now()
         self.siteurl = settings.get('SITEURL')
-
-        self.default_timezone = settings.get('TIMEZONE', 'UTC')
-        self.timezone = getattr(self, 'timezone', self.default_timezone)
-        self.timezone = timezone(self.timezone)
-
-        self.format = 'xml'
-
-        self.changefreqs = {
-            'articles': 'monthly',
-            'indexes': 'daily',
-            'pages': 'monthly'
-        }
-
-        self.priorities = {
-            'articles': 0.5,
-            'indexes': 0.5,
-            'pages': 0.5
-        }
+        self.sitelogo= settings.get('SITE_LOGO')
+        self.summary_max_length= settings.get('SUMMARY_MAX_LENGTH')
 
         config = settings.get('SITEMAP', {})
         self.twitter_consumer_key = settings.get('TWITTER_CONSUMER_KEY')
         self.twitter_consumer_secret = settings.get('TWITTER_CONSUMER_SECRET')
         self.twitter_token_key = settings.get('TWITTER_TOKEN_KEY')
         self.twitter_token_secret = settings.get('TWITTER_TOKEN_SECRET')
+        self.facebook_token = settings.get('FACEBOOK_TOKEN')
 
     def generate_output(self, writer):
         # search latest_article
@@ -65,6 +63,21 @@ class SnsGenerator(object):
                 api.PostUpdate(tweet)
             else:
                 print 'skip tweeting'
+
+            print 'are you sure to share?[Y/n]'
+            input_line = sys.stdin.readline().rstrip()
+            if input_line.lower() in ['y', 'yes']:
+                graph = facebook.GraphAPI(access_token=self.facebook_token)
+                attachment =  {
+                        'name': self.context['SITENAME'].encode('utf-8'),
+                        'link': (self.siteurl + '/' + latest_article_url).encode('utf-8'),
+                        'caption': latest_article.title.encode('utf-8'),
+                        'description': strip_tags(latest_article.content).encode('utf-8'),
+                        'picture': (self.siteurl + '/' + self.sitelogo).encode('utf-8')
+                    }
+                graph.put_wall_post(message=tweet.encode('utf-8'), attachment=attachment)
+            else:
+                print 'skip sharing'
 
 
 def get_generator(generators):
